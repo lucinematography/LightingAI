11459be3edd6e3426749e1d29a395eed67bc4e2f
lucinematography <goranzm@yahoo.com>
2026-09-18 12:36:30 +0200

    Lock Goya and Giotto conventional coverage

--- backend/desisti-catalog-selftest.js
+++ backend/desisti-catalog-selftest.js
@@ -33,6 +33,7 @@ const expected=[
   'desisti-muse-aurea','desisti-muse-aurea-small',
   'desisti-magis-300','desisti-magis-500','desisti-magis-650','desisti-leonardo-1kw','desisti-leonardo-2kw','desisti-leonardo-multipower','desisti-leonardo-5kw','desisti-leonardo-piccolo-10-12kw','desisti-super-leo-10-12kw','desisti-super-leo-20-24kw',
   'desisti-botticelli-1kw','desisti-botticelli-2kw','desisti-botticelli-5kw','desisti-renoir-300','desisti-renoir-500','desisti-renoir-650','desisti-renoir-2kw','desisti-renoir-5kw',
+  'desisti-goya-400w','desisti-goya-575w','desisti-goya-1200w','desisti-goya-2-5-4kw','desisti-goya-6-12kw','desisti-giotto-mk2-tungsten',
   'desisti-rembrandt-200w-mk2','desisti-rembrandt-575w-mk2','desisti-rembrandt-piccolo-1200w','desisti-rembrandt-1-2-2-5kw','desisti-rembrandt-2-5-4kw','desisti-rembrandt-piccolo-6kw','desisti-rembrandt-piccolo-6-12kw-mk2','desisti-rembrandt-12-18kw-mk2',
   'desisti-remington-575w','desisti-remington-1200w','desisti-remington-2-5-4kw','desisti-remington-6kw','desisti-remington-6-12kw'
 ];

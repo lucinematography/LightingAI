82672618fd2ec878526c53959e4e603b97422151
lucinematography <goranzm@yahoo.com>
2026-09-18 12:21:49 +0200

    Wire De Sisti tungsten and HMI families into catalog

--- backend/catalog-runtime.js
+++ backend/catalog-runtime.js
@@ -79,6 +79,8 @@ import { DESISTI_SPACELIGHT_FIXTURES, DESISTI_SPACELIGHT_ACCESSORIES } from './d
 import { DESISTI_LITE_FIXTURES, DESISTI_LITE_ACCESSORIES } from './desisti-lite-series-library.js';
 import { DESISTI_SOFTLED_VWC_FIXTURES, DESISTI_SOFTLED_VWC_ACCESSORIES } from './desisti-softled-vwc-library.js';
 import { DESISTI_MUSES_FIXTURES, DESISTI_MUSES_ACCESSORIES } from './desisti-muses-library.js';
+import { DESISTI_TUNGSTEN_FIXTURES, DESISTI_TUNGSTEN_ACCESSORIES } from './desisti-tungsten-library.js';
+import { DESISTI_HMI_FIXTURES, DESISTI_HMI_ACCESSORIES } from './desisti-hmi-library.js';
 import { applyAccessoryCompatibilityOverrides } from './accessory-compatibility-overrides.js';
 import { applyCatalogCompatibilityCorrections } from './catalog-compatibility-corrections.js';
 import { applyElectroStormCanonicalCorrections } from './electro-storm-canonical-corrections.js';
@@ -126,6 +128,8 @@ export function buildRuntimeCatalog() {
   fixtures.push(...clone(DESISTI_LITE_FIXTURES));
   fixtures.push(...clone(DESISTI_SOFTLED_VWC_FIXTURES));
   fixtures.push(...clone(DESISTI_MUSES_FIXTURES));
+  fixtures.push(...clone(DESISTI_TUNGSTEN_FIXTURES));
+  fixtures.push(...clone(DESISTI_HMI_FIXTURES));
   const kits = [...clone(ARRI_SKYPANEL_KITS), ...clone(ARRI_TUNGSTEN_KITS)];
   const accessoryDefinitions = [...clone(ACCESSORY_LIBRARY), ...clone(ADDITIONAL_ACCESSORY_LIBRARY), ...clone(SPOTLIGHT_ACCESSORY_LIBRARY), ...clone(SPACE_LIGHT_ACCESSORY_LIBRARY), ...clone(STORM_80C_ADAPTED_ACCESSORY_LIBRARY), ...clone(APUTURE_MOUNT_SYSTEM_LIBRARY), ...clone(STORM_SUPPORT_CONTROL_LIBRARY), ...clone(ELECTRO_STORM_TRANSPORT_POWER_LIBRARY), ...clone(ELECTRO_STORM_SYSTEM_ACCESSORY_LIBRARY), ...clone(ARRI_SKYPANEL_X_ACCESSORIES), ...clone(ARRI_SKYPANEL_PRO_ACCESSORIES), ...clone(ARRI_SKYPANEL_CLASSIC_S30_ACCESSORIES), ...clone(ARRI_SKYPANEL_CLASSIC_S60_ACCESSORIES), ...clone(ARRI_SKYPANEL_CLASSIC_S120_ACCESSORIES), ...clone(ARRI_SKYPANEL_CLASSIC_S360_ACCESSORIES), ...clone(ARRI_SKYPANEL_DISCONTINUED_ACCESSORIES), ...clone(ARRI_L_SERIES_PLUS_ACCESSORIES), ...clone(ARRI_ORBITER_ACCESSORIES), ...clone(ARRI_L_SERIES_C_DISCONTINUED_ACCESSORIES), ...clone(ARRI_L_SERIES_DT_TT_DISCONTINUED_ACCESSORIES), ...clone(ARRI_CASTER_SERIES_DISCONTINUED_ACCESSORIES), ...clone(ARRI_M_SERIES_M8_ACCESSORIES), ...clone(ARRI_M_SERIES_M18_ACCESSORIES), ...clone(ARRI_M_SERIES_M40_ACCESSORIES), ...clone(ARRI_M_SERIES_M90_ACCESSORIES), ...clone(ARRI_M_SERIES_ARRIMAX_18_12_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_D5_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_D12_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_D25_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_D40_ACCESSORIES), ...clone(ARRI_DAYLIGHT_18_12_ACCESSORIES), ...clone(ARRI_ARRISUN_DISCONTINUED_ACCESSORIES), ...clone(ARRI_ARRISUN_EVENT_DISCONTINUED_ACCESSORIES), ...clone(ARRI_COMPACT_THEATER_DISCONTINUED_ACCESSORIES), ...clone(ARRI_COMPACT_DISCONTINUED_ACCESSORIES), ...clone(ARRI_ARRILUX_DISCONTINUED_ACCESSORIES), ...clone(ARRI_X_SERIES_DISCONTINUED_ACCESSORIES), ...clone(ARRI_ARRILITE_PLUS_ACCESSORIES), ...clone(ARRI_JUNIOR_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_T1_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_T2_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_T5_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_ST1_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_ST2_3_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_ST5_ACCESSORIES), ...clone(ARRI_TRUE_BLUE_ST_THEATER_ACCESSORIES), ...clone(ARRI_STUDIO_T_ACCESSORIES), ...clone(ARRI_TUNGSTEN_DISCONTINUED_ACCESSORIES)];
   accessoryDefinitions.push(...clone(ASTERA_TITANTUBE_ACCESSORIES));
@@ -158,6 +162,8 @@ export function buildRuntimeCatalog() {
   accessoryDefinitions.push(...clone(DESISTI_LITE_ACCESSORIES));
   accessoryDefinitions.push(...clone(DESISTI_SOFTLED_VWC_ACCESSORIES));
   accessoryDefinitions.push(...clone(DESISTI_MUSES_ACCESSORIES));
+  accessoryDefinitions.push(...clone(DESISTI_TUNGSTEN_ACCESSORIES));
+  accessoryDefinitions.push(...clone(DESISTI_HMI_ACCESSORIES));
   const duplicateAccessoryIds = [];
   const accessoriesById = new Map();
   for (const acc of accessoryDefinitions) {

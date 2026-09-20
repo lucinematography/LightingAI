8d672769abbc2c4ad422c6622c7fad496b6a6d9e
lucinematography <goranzm@yahoo.com>
2026-09-20 12:22:33 +0200

    Add verified EV Light SP600 control route

--- backend/evlight-fresnel-library.js
+++ backend/evlight-fresnel-library.js
@@ -8,6 +8,7 @@ const SP350='https://www.evlightpro.com/fresnel-led/62965541.html';
 const SP350BI='https://www.evlightpro.com/fresnel-led/63234789.html';
 const SP350FC='https://www.evlightprofessional.com/quality-fresnel-led-63172212.html';
 const SP500BI='https://www.evlightprofessional.com/products-index/7/';
+const SP600='https://www.evlightprofessional.com/products-index/2/';
 
 function fixture(id,model,powerW,cctMin,cctMax,colorMode,beamMin,beamMax,sourceUrl,extra={}){
   return {
@@ -146,7 +147,27 @@ export const EVLIGHT_FRESNEL_FIXTURES=[
       sourceUrls:[SP500BI,SP_SERIES]
     }
   }),
-  fixture('evlight-sp600','SP600',600,3000,3000,'Tungsten White',15,55,SP_SERIES,{cri:97,tlci:98})
+  fixture('evlight-sp600','SP600',600,3000,3000,'Tungsten White',15,55,SP600,{
+    cri:97,
+    tlci:98,
+    pwm:'2-25 kHz',
+    zoom:'Manual zoom',
+    dmxChannels:[2],
+    dmxConnection:'3-pin XLR in/out',
+    control:{
+      wired:['DMX512','RDM'],
+      wireless:[],
+      builtInCRMX:false,
+      builtInBluetooth:false,
+      directLightingAI:[],
+      externalInterfaceRequired:['Wired DMX interface for DMX512/RDM control'],
+      unavailableDirectProtocols:[
+        'Official EV Light SP600 product information does not publish Art-Net or sACN network input',
+        'Official EV Light SP600 product information does not publish CRMX/LumenRadio, Bluetooth or Wi-Fi control'
+      ],
+      sourceUrls:[SP600,SP_SERIES]
+    }
+  })
 ];
 
 const all=EVLIGHT_FRESNEL_FIXTURES.map(x=>x.id);
